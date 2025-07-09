d3ff79c1e8a37ad9d0ba6cc73c759215fa9317d2
Shivarama-B-N_bdgh <shivarama.b.n@bd.com>
2025-07-09 12:54:13 +0530

    Add charts

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -2,26 +2,36 @@ import { Component, OnInit, ChangeDetectorRef, ElementRef } from '@angular/core'
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { Color, ScaleType } from '@swimlane/ngx-charts';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import * as data from '../data/bar-graph.json';
 
 @Component({
   selector: 'app-bar-chart',
   standalone: true,
-  imports: [NgxChartsModule, CommonModule],
+  imports: [NgxChartsModule, CommonModule, FormsModule],
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.css']
 })
 export class BarChartComponent implements OnInit {
   chartData = data;
   mainChartData: any[] = [];
+  originalChartData: any[] = [];
   chartView: [number, number] = [0, 0];
   barHeight = 40;
+  
+
+  availableYears: string[] = [];
+  yearSelection: { [year: string]: boolean } = {};
+  showDataLabels: boolean = false; 
+  legend: boolean = true// ✅ Added this line
+
   colorScheme: Color = {
     name: 'custom',
     group: ScaleType.Ordinal,
     selectable: true,
     domain: ['#0288d1', '#c2185b', '#fbc02d', '#7b1fa2']
   };
+
   private resizeObserver!: ResizeObserver;
   private chartContainer!: HTMLElement | null;
 
@@ -43,12 +53,14 @@ export class BarChartComponent implements OnInit {
   formatChartData() {
     const devices = ['PICC', 'CVC', 'Port', 'HD'];
     const seriesMap = new Map<string, any>();
+    const yearsSet = new Set<string>();
 
     devices.forEach(device => {
       seriesMap.set(device, { name: device, series: [] });
     });
 
     this.chartData.clabsiOccurrenceByDeviceType.forEach((yearData: any) => {
+      yearsSet.add(yearData.year.toString());
       yearData.details.forEach((entry: any) => {
         const group = seriesMap.get(entry.field);
         if (group) {
@@ -57,7 +69,19 @@ export class BarChartComponent implements OnInit {
       });
     });
 
-    this.mainChartData = Array.from(seriesMap.values());
+    this.availableYears = Array.from(yearsSet).sort();
+    this.availableYears.forEach(year => this.yearSelection[year] = true);
+
+    this.originalChartData = Array.from(seriesMap.values());
+    this.filterChartData();
+  }
+
+  filterChartData() {
+    const selectedYears = this.availableYears.filter(year => this.yearSelection[year]);
+    this.mainChartData = this.originalChartData.map(device => ({
+      name: device.name,
+      series: device.series.filter((s: any) => selectedYears.includes(s.name))
+    }));
     this.updateChartView();
   }
 
@@ -77,8 +101,7 @@ export class BarChartComponent implements OnInit {
   }
 
   getColor(name: string): string {
-  const index = this.mainChartData.findIndex(item => item.name === name);
-  return this.colorScheme.domain[index % this.colorScheme.domain.length];
+    const index = this.mainChartData.findIndex(item => item.name === name);
+    return this.colorScheme.domain[index % this.colorScheme.domain.length];
+  }
 }
-}
-
--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -1,35 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxChartsModule, LegendPosition } from '@swimlane/ngx-charts';
+import { NgxChartsModule, LegendPosition,ScaleType } from '@swimlane/ngx-charts';
 import { CommonModule } from '@angular/common';
-import * as data from '../data/pie-chart.json';
+import * as rawData from '../data/pie-chart.json'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pie-chart',
   standalone: true,
-  imports: [NgxChartsModule, CommonModule],
+  imports: [NgxChartsModule, CommonModule,FormsModule],
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.css'],
 })
 export class PieChartComponent implements OnInit {
-  pieData: any[] = [];
   chartView: [number, number] = [700, 700];
-
   legend: boolean = true;
   showLabels: boolean = true;
   legendPosition: LegendPosition = LegendPosition.Below;
 
+ 
+colorToggle: boolean = false;
 
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
-  };
+multiColorScheme = {
+  name: 'multiColor',
+  selectable: true,
+  group: ScaleType.Ordinal,
+  domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA', '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728'],
+};
+
+monoColorScheme = {
+  name: 'monoColor',
+  selectable: true,
+  group: ScaleType.Ordinal,
+  domain: ['#003f5c', '#2f4b7c', '#665191', '#a05195', '#d45087', '#f95d6a', '#ff7c43', '#ffa600'],
+};
+
+get colorScheme() {
+  return this.colorToggle ? this.monoColorScheme : this.multiColorScheme;
+}
+
+toggleColorScheme() {
+  this.colorToggle = !this.colorToggle;
+}
 
  
-ngOnInit() {
-  this.pieData = (data as any).default;
+  infectionSourceData: any[] = [];
 
-  // Adjust view size based on screen width
-  const screenWidth = window.innerWidth;
-  this.chartView = screenWidth < 600 ? [300, 300] : [700, 700];
-}
-}
+  ngOnInit() {
+    const data = (rawData as any).default;
+    this.infectionSourceData = this.transformToChartData(data["By Infection Source"]);
 
+    const screenWidth = window.innerWidth;
+   
+this.chartView = screenWidth < 600 ? [300, 300] : [700, 700];
+
+  }
+
+  transformToChartData(obj: { [key: string]: number }): any[] {
+    return Object.entries(obj).map(([name, value]) => ({ name, value }));
+  }
+}
